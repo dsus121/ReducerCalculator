c8692326ddb34df37ec603d40ca260c6ee0f5a05
Dawn Cheairs <dawnsusanne@gmail.com>
2025-02-24 11:17:41 -0700

    Functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useReducer, useRef } from 'react';
 import './App.css'
 
+function reducer(state, action) {
+  let num1 = Number(action.payload[0])
+  let num2 = Number(action.payload[1])
+  console.log(action)
+  switch(action.type) {
+    case 'ADD':
+      return num1 + num2
+    case 'SUBTRACT':
+      return num1 - num2
+    case 'MULTIPLY':
+      return num1 * num2
+    case 'DIVIDE':
+      return num1 / num2
+    case 'CLEAR':
+      return 0
+    default: 
+  return state
+  }
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+
+const firstInputRef = useRef()
+const secondInputRef = useRef() // { current: input }
+
+const [result, dispatch] = useReducer(reducer, 0) // function, initial state
+
+function handleOperation(operation) {
+  if (operation === 'CLEAR') {
+    firstInputRef.current.value = ''
+    secondInputRef.current.value = ''
+  }
+  dispatch({
+    type: operation,
+    payload: [
+      firstInputRef.current.value,
+      secondInputRef.current.value
+    ]
+  })
+}
 
   return (
-    <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input type="number" ref={firstInputRef} />
+        <input type="number" ref={secondInputRef} />  
+          <div>
+          <button onClick={() => handleOperation('ADD')}>Add</button>
+          <button onClick={() => handleOperation('SUBTRACT')}>Subtract</button>
+          <button onClick={() => handleOperation('MULTIPLY')}>Multiply</button>
+          <button onClick={() => handleOperation('DIVIDE')}>Divide</button>
+          <button onClick={() => handleOperation('CLEAR')}>Clear</button>
+          <br />
+          {result}
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  );
+};
+
 
 export default App
